a86d3762d5f00ae9b21bcb7b78f3a1323043e888
Bima Rizqy Ramadhan <bimarizqyramadhan@gmail.com>
2025-10-02 15:12:04 +0700

    try without env

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.post('/:message', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT
+// const PORT = process.env.PORT
+const PORT = 8080
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
